0fb3c382cbb2f6a0dc3152d71bcadbd8e616e761
Akira Vernon <akirav@hawaii.edu>
2018-10-11 23:30:18 -1000

    pyqt app not working

---
+++ mqtt/image_sub_base64.py
@@ -0,0 +1,40 @@
+import paho.mqtt.client as mqtt
+import numpy as np
+import cv2
+import base64
+
+MQTT_SERVER = "168.105.242.233"
+MQTT_PATH = "test_channel"
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print "Connected with result code "+str(rc)
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(MQTT_PATH)
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    #print msg.topic+" sent "+str(msg.payload)
+
+    nparr = np.fromstring(msg.payload.decode('base64'), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
+
+    print "Recieved and Decoded"
+    cv2.imshow('From Source', img)
+
+    cv2.waitKey(1)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(MQTT_SERVER, 1883, 60)
+
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
