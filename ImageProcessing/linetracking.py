da1b9bd88b871ae411cfd6a73f96897c3adcfd2a
arnoldshek <ashek@hawaii.edu>
2018-10-19 12:15:07 -1000

    Base functionality added for road tracking

--- ImageProcessing/linetracking.py
+++ ImageProcessing/linetracking.py
@@ -1,79 +1,123 @@
 import numpy as np
 import cv2
+from picamera import PiCamera
+import time
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+from picamera.array import PiRGBArray
 #cap = cv2.VideoCapture('silent.mp4')
 #cap = cv2.VideoCapture('Video.MOV')
-cap = cv2.VideoCapture('duckie_vid.mp4')
+#cap = cv2.VideoCapture('duckie_vid.mp4')
 #raw = cv2.imread('screen-shot.png');
-'''
-    window_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    window_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 20
+capture = PiRGBArray(camera, size=(640,480))
+time.sleep(0.1)
+#'''
+ #   window_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+  #  window_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-    print "Width: " + str(window_width)
-    print "Height: " + str(window_height)
-    '''
+#    print "Width: " + str(window_width)
+ #   print "Height: " + str(window_height)
+  #  '''
 
 window_width = 480
 window_height = 360
 ROI_road_offset = int(0.66*window_height)
+mh = Adafruit_MotorHAT(addr=0x60)
+leftMotor = mh.getMotor(2)
+rightMotor = mh.getMotor(1)
+leftMotor.run(Adafruit_MotorHAT.FORWARD)
+rightMotor.run(Adafruit_MotorHAT.FORWARD)
 
-while(True):
-    ret, raw = cap.read()
-    raw = cv2.resize(raw, (window_width, window_height))
+#rightMotor.setSpeed(int(255))
+#leftMotor.setSpeed(int(255))
+def position_controller(kp, target, actual):
+    error = actual-target
+    #print 'Error = ',error
+    if error < 5:
+        #do nothing
+        return int(0)
+    else:
+        adjustment = kp*error
+        return int(adjustment)
+try:
+    for frame in camera.capture_continuous(capture, format='bgr', use_video_port=True):
+       #ret, raw = cap.read()
+        image = capture.array
+        raw = cv2.resize(image, (window_width, window_height))
 
-    #rotate image
- #   rows,cols,_ = raw.shape
-  #  rawrot = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
+#    rotate image
+    #rows,cols,_ = raw.shape
+    #rawrot = cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
     #raw = cv2.warpAffine(raw,rawrot,(cols,rows))
 
 
-    finished = cv2.resize(raw, (window_width, window_height))
+    #finished = cv2.resize(raw, (window_width, window_height))
 #   ROI?
     #finished = finished[0:200,0:480]
     
-    frame = cv2.GaussianBlur(finished, (5, 5), 0)
-    Sum = 0
-    numx = 0
+        frame = cv2.GaussianBlur(raw, (5, 5), 0)
+        Sum = 0
+        numx = 0
+        avg = 0
     #road = raw[ROI_road_offset:window_height, 0:window_width]
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    min_yellow = np.array([18, 94, 140])
-    max_yellow = np.array([48, 255, 255])
-    upper = np.array([101, 13, 255])
-    lower = np.array([3, 0, 229])
-    mask1 = cv2.inRange(hsv, lower, upper)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        min_yellow = np.array([18, 94, 140])
+        max_yellow = np.array([48, 255, 255])
+        upper = np.array([101, 13, 255])
+        lower = np.array([3, 0, 229])
+        mask1 = cv2.inRange(hsv, lower, upper)
     #cv2.imshow("white mask", mask1)
 
 
-    mask2 = cv2.inRange(hsv, min_yellow, max_yellow)
-    mask = cv2.bitwise_or(mask2, mask1)
-    cv2.imshow(" mask", mask2)
-    masked_img = cv2.bitwise_and(hsv, hsv, mask=mask2)
-    H, S, V = cv2.split(masked_img)
-    edges = cv2.Canny(mask2, 50, 150, apertureSize=3)
+        mask2 = cv2.inRange(hsv, min_yellow, max_yellow)
+        mask = cv2.bitwise_or(mask2, mask1)
+        cv2.imshow(" mask", mask2)
+        masked_img = cv2.bitwise_and(hsv, hsv, mask=mask2)
+        H, S, V = cv2.split(masked_img)
+        edges = cv2.Canny(mask2, 50, 150, apertureSize=3)
     #road = edges[260:360, 0:480]
 
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180,100, minLineLength= 50, maxLineGap=10)
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.circle(frame, (x1, y1),2,(255,0,0),3)
-            cv2.circle(frame,(x2,y2),2,(255,0,0),3)
-            numx += 2
-            Sum = Sum + x1 + x2
-
-    avg = Sum/numx
-    cv2.circle(frame,(avg,300),2,(0,0,255),3)
-    #print avg
-    cv2.imshow('frame', frame)
-    cv2.imshow('edges', edges)
+        lines = cv2.HoughLinesP(edges, 1, np.pi/180,100, minLineLength= 50, maxLineGap=10)
+        if lines is not None:
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.circle(frame, (x1, y1),2,(255,0,0),3)
+                cv2.circle(frame,(x2,y2),2,(255,0,0),3)
+                numx += 2
+                Sum = Sum + x1 + x2
+        old_avg = avg
+        if numx is not 0:
+            avg = Sum/numx
+        else:
+            avg = old_avg
+        cv2.circle(frame,(avg,300),2,(0,0,255),3)
+        print 'Average = ', avg
+        cv2.imshow('frame', frame)
+        cv2.imshow('edges', edges)
 #cv2.imshow('road', road)
 
 #overlay = cv2.line(frame,(int(window_width/2),0),(int(window_width/2), int(window_height)),(0,0,255),5)
 #cv2.imshow('overlay', overlay)
 
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+    
+        rightspeed = int(115 + position_controller(0.1,180,avg))
+        if rightspeed > 255:
+            rightspeed = 255
+    
+        elif rightspeed < 0:
+            rightspeed = 0
 
-cap.release()
+        rightMotor.setSpeed(rightspeed)
+        leftMotor.setSpeed(125)
+        print 'Rightspeed = ',rightspeed 
+        capture.truncate(0)
+except KeyboardInterrupt:
+	leftMotor.run(Adafruit_MotorHAT.RELEASE)
+	rightMotor.run(Adafruit_MotorHAT.RELEASE)
 cv2.destroyAllWindows()
