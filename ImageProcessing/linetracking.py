aacaa33527e08605403314c297a9c9692769f652
Anthony <amartin3@hawaii.edu>
2018-10-18 18:44:47 -1000

    Added code to find average of points on yellow line

--- ImageProcessing/linetracking.py
+++ ImageProcessing/linetracking.py
@@ -2,11 +2,11 @@ import numpy as np
 import cv2
 #cap = cv2.VideoCapture('silent.mp4')
 #cap = cv2.VideoCapture('Video.MOV')
-raw = cv2.imread('still-image.jpg');
+raw = cv2.imread('screen-shot.png');
 '''
     window_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     window_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    
+
     print "Width: " + str(window_width)
     print "Height: " + str(window_height)
     '''
@@ -18,11 +18,13 @@ ROI_road_offset = int(0.66*window_height)
 while(True):
     #ret, raw = cap.read()
     #raw = cv2.resize(raw, (window_width, window_height))
+
     finished = cv2.resize(raw, (window_width, window_height))
     frame = cv2.GaussianBlur(finished, (5, 5), 0)
-    
+    Sum = 0
+    numx = 0
     #road = raw[ROI_road_offset:window_height, 0:window_width]
-    
+
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     min_yellow = np.array([18, 94, 140])
     max_yellow = np.array([48, 255, 255])
@@ -30,23 +32,29 @@ while(True):
     lower = np.array([3, 0, 229])
     mask1 = cv2.inRange(hsv, lower, upper)
     #cv2.imshow("white mask", mask1)
-    
-    
+
+
     mask2 = cv2.inRange(hsv, min_yellow, max_yellow)
     mask = cv2.bitwise_or(mask2, mask1)
-    cv2.imshow(" mask", mask)
-    masked_img = cv2.bitwise_and(hsv, hsv, mask=mask)
+    cv2.imshow(" mask", mask2)
+    masked_img = cv2.bitwise_and(hsv, hsv, mask=mask2)
     H, S, V = cv2.split(masked_img)
-    edges = cv2.Canny(mask, 50, 150, apertureSize=3)
-    
+    edges = cv2.Canny(mask2, 50, 150, apertureSize=3)
+    #road = edges[260:360, 0:480]
+
     lines = cv2.HoughLinesP(edges, 1, np.pi/180,100, minLineLength= 50, maxLineGap=10)
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line[0]
             cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.circle(frame, (x1, y1),2,(255,0,0),3);
-            cv2.circle(frame,(x2,y2),2,(255,0,0),3);
+            cv2.circle(frame, (x1, y1),2,(255,0,0),3)
+            cv2.circle(frame,(x2,y2),2,(255,0,0),3)
+            numx += 2
+            Sum = Sum + x1 + x2
 
+    avg = Sum/numx
+    cv2.circle(frame,(avg,300),2,(0,0,255),3)
+    #print avg
     cv2.imshow('frame', frame)
     cv2.imshow('edges', edges)
 #cv2.imshow('road', road)
