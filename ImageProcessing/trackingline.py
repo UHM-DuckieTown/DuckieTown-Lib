f97c063521f163a8e19e27e1152c14842ea26d43
charles <cdrafael@hawaii.edu>
2018-11-08 16:14:00 -1000

    Fixed indenting error

--- ImageProcessing/trackingline.py
+++ ImageProcessing/trackingline.py
@@ -28,33 +28,35 @@ def trackingline(raw):
 	if np.all(cv2.bitwise_not(mask2)) == False:
 		mask = mask2
 		yellow = True
+
 	else:
 		mask = mask1
 		yellow = False
-        cv2.imshow(" mask", mask)
-        masked_img = cv2.bitwise_and(hsv, hsv, mask=mask)
-        H, S, V = cv2.split(masked_img)
-        edges = cv2.Canny(mask, 50, 150, apertureSize=3)
+
+    cv2.imshow(" mask", mask)
+    masked_img = cv2.bitwise_and(hsv, hsv, mask=mask)
+    H, S, V = cv2.split(masked_img)
+    edges = cv2.Canny(mask, 50, 150, apertureSize=3)
     #road = edges[260:360, 0:480]
 
-        lines = cv2.HoughLinesP(edges, 1, np.pi/180,20, minLineLength= 1, maxLineGap=1)
-        if lines is not None:
-            for line in lines:
-                x1, y1, x2, y2 = line[0]
-                cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.circle(frame, (x1, y1),2,(255,0,0),3)
-                cv2.circle(frame,(x2,y2),2,(255,0,0),3)
-                numx += 2
-                Sum = Sum + x1 + x2
-        old_avg = avg
-        if numx is not 0:
-            avg = Sum/numx
-        else:
-            avg = old_avg
-        cv2.circle(frame,(avg,300),2,(0,0,255),3)
-        print 'Average = ', avg
-        cv2.imshow('frame', frame)
-        cv2.imshow('edges', edges)
-        if cv2.waitKey(20) & 0xFF == ord('q'):
-            break
-        return yellow,avg
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180,20, minLineLength= 1, maxLineGap=1)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.circle(frame, (x1, y1),2,(255,0,0),3)
+            cv2.circle(frame,(x2,y2),2,(255,0,0),3)
+            numx += 2
+            Sum = Sum + x1 + x2
+    old_avg = avg
+    if numx is not 0:
+        avg = Sum/numx
+    else:
+        avg = old_avg
+    cv2.circle(frame,(avg,300),2,(0,0,255),3)
+    print 'Average = ', avg
+    cv2.imshow('frame', frame)
+    cv2.imshow('edges', edges)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+    return yellow,avg
