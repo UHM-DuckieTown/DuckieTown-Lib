a56cdf1a3eaa1e3057dbf38658652df034065b39
Jon Lau <laujh@hawaii.edu>
2018-11-08 20:49:23 -1000

    video streaming

--- ImageProcessing/lbp_matching/testing.py
+++ ImageProcessing/lbp_matching/testing.py
@@ -10,29 +10,23 @@ from sklearn.externals import joblib
 import argparse as ap
 import cvutils
 
-# ARGUMENTS
-parser = ap.ArgumentParser()
-parser.add_argument("-t", "--testingSet", help="Path to Testing Set", required="True")
-parser.add_argument("-l", "--imageLabels", help="Path to Image Label Files", required="True")
-args = vars(parser.parse_args())
-
-
 #load training result histograms
 X_name, X_test, y_test = joblib.load("lbp.pkl")
 
-test_images = cvutils.imlist(args["testingSet"])
 test_dic = {}
-with open(args["imageLabels"], 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ')
-    for row in reader:
-        test_dic[row[0]] = int(row[1])
 
 # Dict containing scores
 results_all = {}
 
-for test_image in test_images:
-    print "\nCalculating Hist:  " + str(test_image)
-    im = cv2.imread(test_image)
+video = '../ball.avi'
+
+camera = cv2.VideoCapture(video)
+
+while True:
+    ret, im = camera.read()
+
+    if not ret:
+        break
     im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     radius = 3
     no_points = 8 * radius
@@ -44,7 +38,5 @@ for test_image in test_images:
         score = cv2.compareHist(np.array(x, dtype=np.float32), np.array(hist, dtype=np.float32), cv2.cv.CV_COMP_CHISQR)
         results.append((X_name[index], round(score, 3)))
     results = sorted(results, key=lambda score: score[1])
-    results_all[test_image] = results
-    print "Scores: " + str(test_image)
     for image, score in results:
         print "{} has score {}".format(image, score)
