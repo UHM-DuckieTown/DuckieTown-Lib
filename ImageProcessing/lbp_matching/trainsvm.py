fe850d4e85daa5e5423f10df01b45c3362060268
Andy Chu <achu6@hawaii.edu>
2018-11-16 19:34:08 -1000

    stop model v1

--- ImageProcessing/lbp_matching/trainsvm.py
+++ ImageProcessing/lbp_matching/trainsvm.py
@@ -10,6 +10,7 @@ from sklearn.externals import joblib
 import argparse as ap
 import cvutils
 import csv
+import matplotlib.pyplot as plt
 
 def lbp(train_images, lbpset):
     #itterate through train images
@@ -17,21 +18,26 @@ def lbp(train_images, lbpset):
         #open image
         im = cv2.imread(train_image)
         #convert to grayscale image
-        im_gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+        im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         #set pts & radius
         radius = 1
-        no_points = 8 * radius
+        no_points = 8
         #get lbp image
         lbp = local_binary_pattern(im_gray, no_points, radius, method='uniform')
+        #cv2.imshow(lbp)
         #sort by freq
-        x = itemfreq(lbp.ravel())
+        x = lbp.ravel()
+        #print(x)
         # Normalize values
-        hist = x[:, 1]/sum(x[:, 1])
+        #hist = x[:, 1]/sum(x[:, 1])
         #set default hist
-        hist = np.histogram(hist,bins =20, range =(0,1))
-        #print(list(hist[0]))
+        hist, _ = np.histogram(x,bins = (no_points+2), range =(0,no_points+2), density = True)
+        #plt.hist(x, bins='auto')
+        #plt.show()
+        #print(list(hist))
+        #print(sum(list(hist)))
         #add values to set
-        lbpset.append(list(hist[0]))
+        lbpset.append(list(hist))
 
 
 
@@ -56,27 +62,6 @@ lbp(posSet, lbpset)
 #X is the array of the points while Y is the label of whether it's true or not
 X_train, X_test, y_train, y_test = train_test_split(lbpset, labels, test_size = 0.2, random_state=0)
 
-#print("Displaying data. Close window to continue")
-# Plot data
-#plot_data(X_train, y_train, X_test, y_test)
-
-#print("Training SVM ...")
-# make a classifier
-#clf = svm.SVC(C = 10.0, kernel='rbf', gamma=0.1)
-
-# Train classifier
-#clf.fit(X_train, y_train)
-
-# Make predictions on unseen test data
-#clf_predictions = clf.predict(X_test)
-
-#print("Displaying decision function. Close window to continue")
-# Plot decision function on training and test data
-#plot_decision_function(X_train, y_train, X_test, y_test, clf)
-
-# Grid Search
-#print("Performing grid search ... ")
-
 # Parameter Grid
 param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001, 0.00001, 10]}
 
@@ -86,10 +71,7 @@ clf_grid = GridSearchCV(svm.SVC(), param_grid, verbose=1)
 # Train the classifier
 clf_grid.fit(X_train, y_train)
 
-# clf = grid.best_estimator_()
+
 print("Best Parameters:\n", clf_grid.best_params_)
 print("Best Estimators:\n", clf_grid.best_estimator_)
-joblib.dump(clf_grid, "clf_grid")
-#print("Displaying decision function for best estimator. Close window to continue.")
-# Plot decision function on training and test data
-#plot_decision_function(X_train, y_train, X_test, y_test, clf_grid)
+joblib.dump(clf_grid, "clf_grid_Stop")
