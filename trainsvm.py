6533cdad84d0c1b2be88f30e405b3e5fe4953348
arnoldshek <ashek@hawaii.edu>
2019-01-22 19:53:26 -1000

    added drawers negatives

--- trainsvm.py
+++ trainsvm.py
@@ -69,10 +69,10 @@ X_train, X_test, y_train, y_test = train_test_split(lbpset, labels, test_size = 
 #gamma_range = np.logspace(-9, 3, 13)
 #param_grid = dict(gamma=gamma_range, C=C_range)
 
-param_grid = {'C': [0.01, 0.1, 1, 5, 10], 'gamma': [5, 1, 0.1, 0.01, 0.001, 0.00001]}
+param_grid = {'C': [20, 30, 40], 'gamma': [20, 30, 40]}
 
 # Make grid search classifier
-clf_grid = GridSearchCV(svm.SVC(probability=True, kernel='linear'), param_grid, verbose=1)
+clf_grid = GridSearchCV(svm.SVC(probability=True, kernel='poly'), param_grid, verbose=1)
 
 #clf_grid.probability = True
 
