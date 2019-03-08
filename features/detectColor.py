daa3fc8c64109437dfb6163b7704174651951f1f
amartin3 <amartin3@hawaii.edu>
2019-03-07 14:44:40 -1000

    draw box around contour

--- features/detectColor.py
+++ features/detectColor.py
@@ -10,39 +10,28 @@ import imutils
 def find_shape(image):
     # convert color scheme
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    max_dim = max(image.shape)
-
-    # scale image
-    scale = 1
-    image = cv2.resize(image, None, fx=scale, fy=scale)
-    resized = imutils.resize(image, width=300)
-    ratio = image.shape[0] / float(resized.shape[0])
-
-    # smooth image (image, ratio, filter)
-    image_blur = cv2.GaussianBlur(resized, (7,7), 0)
+    
+    image_blur = cv2.GaussianBlur(image, (7,7), 0)
     
     #hsv - sepparates color from brightness
     image_blur_hsv = cv2.cvtColor(image_blur, cv2.COLOR_RGB2HSV)
 
     # filter by color range
     # filter out black
-    lowerBlack = np.array([100,50,50])
-    upperBlack = np.array([130,255,255])
-    mask = cv2.inRange(image_blur_hsv, lowerBlack, upperBlack)
+    lower = np.array([100,50,50])
+    upper = np.array([130,255,255])
+    mask = cv2.inRange(image_blur_hsv, lower, upper)
 
     _, contours, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     print "num contours detected: {}".format(len(contours))
     c= max(contours, key = cv2.contourArea)
+
+    min_y, min_x, _ = image_blur_hsv.shape
+    max_x = max_y = 0
+    (x,y,w,h) = cv2.boundingRect(c)
     
-    # multiply the contour (x, y)-coordinates by the resize ratio,
-    # then draw the contours and the name of the shape on the image
-    c = c.astype("float")
-    c *= ratio
-    c = c.astype("int")
-    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-        
+    cv2.rectangle(image, (x,y), (x+w,y+h), (255, 0, 0), 2)  
         
     # show the output image
     cv2.imshow("Image", image)
