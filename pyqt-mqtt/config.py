2d9c7bc39a4ec1e7db9332ea4b7ff050154a42dd
Akira Vernon <akirav@hawaii.edu>
2019-04-29 11:27:39 -1000

    added config

---
+++ pyqt-mqtt/config.py
@@ -0,0 +1,17 @@
+import socket
+
+ip_duck = '';
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8",80))
+ip_duck = s.getsockname()[0]
+duck1_feed1 = ip_duck + "_feed1"
+#Secondary Feeds to show Raw, Edges, Line Tracker Image, White, and Yellow Mask
+duck1_feed2 = ip_duck + "_feed2"
+#Subscribed-Topics
+duck1_text = ip_duck + "_text"
+duck1_slider = ip_duck + "_slider"
+
+#def getter():
+#    return test
+
+#Headers in messages
